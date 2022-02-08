c36ec8b518b227308312922691ff947ed5a22bfd
AlaminUI <alaminislam907@gmail.com>
2022-02-09 00:01:58 +0600

    upload

---
+++ preplanafuneral.com/data.js
@@ -0,0 +1,30 @@
+let listing_data= [
+    
+        {
+            "parameter": "qr001",
+            "facebookReview" : "https://www.facebook.com/fb565747703482508/reviews",
+            "GoogleReview":"https://www.google.com/search?q=ideacloud.co&sxsrf=APq-WBty33fjb9TzQ6qEumpyXDk5huV_Hw%3A1644340628613&ei=lKUCYobXJOSdseMP_eCD0AI&ved=0ahUKEwiG8bqYzvD1AhXkTmwGHX3wACoQ4dUDCA4&uact=5&oq=ideacloud.co&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEB4yBAgAEB4yBAgAEB4yCAgAEAgQChAeOgcIABBHELADOgcIABCwAxBDSgUIPBIBMUoECEEYAEoECEYYAFDqB1jqB2CHDmgBcAJ4AIABbIgBbJIBAzAuMZgBAKABAqABAcgBCsABAQ&sclient=gws-wiz#lrd=0x8752f53cb95aaaab:0xac6e8eaef4dc49b0,3,,,"
+          },
+        {
+            "parameter": "qr002",
+            "facebookReview" : "https://www.facebook.com/KFC-Bangladesh-295749957656741/reviews/?ref=page_internal",
+            "GoogleReview":"https://www.google.com/search?q=restaurants%20dhaka&oq=resturant&aqs=chrome.2.69i57j0i10i433i457j0i402l2j0i10l2j69i60l2.4748j0j4&sourceid=chrome&ie=UTF-8&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:9&tbm=lcl&sxsrf=APq-WBsP-vBpbzwJ4DfoDYwJ8McHOZhOEw:1644343025206&rflfq=1&num=10&rldimm=15742330925099032739&lqi=ChFyZXN0YXVyYW50cyBkaGFrYUiG05zL9rGAgAhaHxAAGAAYASIRcmVzdGF1cmFudHMgZGhha2EqBAgDEACSAQpyZXN0YXVyYW50qgETEAEqDyILcmVzdGF1cmFudHMoAA&phdesc=Uf_hFTyDbmY&ved=2ahUKEwirt5-P1_D1AhXdSGwGHWy7DnoQvS56BAgCEFo&rlst=f#lrd=0x3755c7f800bd6b01:0xda77fe91e72ca0a3,3,,,&rlfi=hd:;si:15742330925099032739,l,ChFyZXN0YXVyYW50cyBkaGFrYUiG05zL9rGAgAhaHxAAGAAYASIRcmVzdGF1cmFudHMgZGhha2EqBAgDEACSAQpyZXN0YXVyYW50qgETEAEqDyILcmVzdGF1cmFudHMoAA,y,Uf_hFTyDbmY;mv:[[23.842501199999997,90.4374406],[23.7275472,90.377306]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:9"
+          },
+        {
+            "parameter": "qr003",
+            "facebookReview" : "https://www.facebook.com/Cafe-Darbar-Uttara-195400004531568/reviews/?ref=page_internal",
+            "GoogleReview" : "https://www.google.com/search?q=restaurants%20dhaka&oq=resturant&aqs=chrome.2.69i57j0i10i433i457j0i402l2j0i10l2j69i60l2.4748j0j4&sourceid=chrome&ie=UTF-8&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:9&tbm=lcl&sxsrf=APq-WBsP-vBpbzwJ4DfoDYwJ8McHOZhOEw:1644343025206&rflfq=1&num=10&rldimm=15742330925099032739&lqi=ChFyZXN0YXVyYW50cyBkaGFrYUiG05zL9rGAgAhaHxAAGAAYASIRcmVzdGF1cmFudHMgZGhha2EqBAgDEACSAQpyZXN0YXVyYW50qgETEAEqDyILcmVzdGF1cmFudHMoAA&phdesc=Uf_hFTyDbmY&ved=2ahUKEwirt5-P1_D1AhXdSGwGHWy7DnoQvS56BAgCEFo&rlst=f#lrd=0x3755c7a31885ba6d:0xeafe3e43609233f3,1,,,&rlfi=hd:;si:16933040108063568883,l,ChFyZXN0YXVyYW50cyBkaGFrYUjn9I22oa6AgAhaHxAAGAAYASIRcmVzdGF1cmFudHMgZGhha2EqBAgDEACSAQpyZXN0YXVyYW50qgETEAEqDyILcmVzdGF1cmFudHMoAA,y,6y_OUoxgwFg;mv:[[23.842501199999997,90.4374406],[23.7275472,90.377306]]"
+          },
+        {
+            "parameter": "qr004",
+            "facebookReview" : "https://www.facebook.com/chefstablebd/reviews/?ref=page_internal",
+            "GoogleReview":"https://www.google.com/search?q=ideacloud.co&sxsrf=APq-WBty33fjb9TzQ6qEumpyXDk5huV_Hw%3A1644340628613&ei=lKUCYobXJOSdseMP_eCD0AI&ved=0ahUKEwiG8bqYzvD1AhXkTmwGHX3wACoQ4dUDCA4&uact=5&oq=ideacloud.co&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEB4yBAgAEB4yBAgAEB4yCAgAEAgQChAeOgcIABBHELADOgcIABCwAxBDSgUIPBIBMUoECEEYAEoECEYYAFDqB1jqB2CHDmgBcAJ4AIABbIgBbJIBAzAuMZgBAKABAqABAcgBCsABAQ&sclient=gws-wiz#lrd=0x8752f53cb95aaaab:0xac6e8eaef4dc49b0,3,,,"
+          },
+        {
+            "parameter": "qr005",
+            "facebookReview" : "https://www.facebook.com/Cafe-Darbar-Uttara-195400004531568/reviews/?ref=page_internal",
+            "GoogleReview" : "https://www.google.com/search?q=restaurants%20dhaka&oq=resturant&aqs=chrome.2.69i57j0i10i433i457j0i402l2j0i10l2j69i60l2.4748j0j4&sourceid=chrome&ie=UTF-8&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:9&tbm=lcl&sxsrf=APq-WBsP-vBpbzwJ4DfoDYwJ8McHOZhOEw:1644343025206&rflfq=1&num=10&rldimm=15742330925099032739&lqi=ChFyZXN0YXVyYW50cyBkaGFrYUiG05zL9rGAgAhaHxAAGAAYASIRcmVzdGF1cmFudHMgZGhha2EqBAgDEACSAQpyZXN0YXVyYW50qgETEAEqDyILcmVzdGF1cmFudHMoAA&phdesc=Uf_hFTyDbmY&ved=2ahUKEwirt5-P1_D1AhXdSGwGHWy7DnoQvS56BAgCEFo&rlst=f#lrd=0x3755c7b75cf3202d:0x489061868703d110,1,,,&rlfi=hd:;si:5228786397795832080,l,ChFyZXN0YXVyYW50cyBkaGFrYUjd39qI4LKAgAhaHxAAGAAYASIRcmVzdGF1cmFudHMgZGhha2EqBAgDEACSARRmYXN0X2Zvb2RfcmVzdGF1cmFudKoBExABKg8iC3Jlc3RhdXJhbnRzKAA,y,6LBgJFUzgec;mv:[[23.842501199999997,90.4374406],[23.7275472,90.377306]]"
+          },
+
+    
+]
